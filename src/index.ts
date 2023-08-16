34372dbab884d96014939ff00eac1a690a9e3c8d
MikuroXina <ryosukadnak@gmail.com>
2023-08-16 12:53:49 +0900

    Add more envs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 
 type Bindings = {
     ASSOC_BUCKET: R2Bucket;
+    DISCORD_CLIENT_ID: string;
+    DISCORD_CLIENT_SECRET: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
