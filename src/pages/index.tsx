e3fb70001a6bf0b2cdbc56d7aad472e04b435294
MikuroXina <ryosukadnak@gmail.com>
2023-08-16 16:20:38 +0900

    Format Index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
-export const Index = (): JSX.Element => <div>
-    <h1>Hello Hono!</h1>
-</div>;
+export const Index = (): JSX.Element => (
+    <html lang="ja">
+        <head>
+            <title>Approvers メンバー情報登録</title>
+        </head>
+        <body>
+            <h1>Approvers メンバー情報登録</h1>
+            <h2>Approvers へようこそ</h2>
+            <p>こちらではあなたの Discord アカウントに関連付けてある GitHub / X (旧 Twitter) アカウントの情報を登録できます. これは Discord の OAuth を利用しており, 下記のボタンから当サービスでのトークンの利用を認可していただくことで登録処理を開始できます.</p>
+            <button>アカウント情報を登録する</button>
+        </body>
+    </html>
+);
