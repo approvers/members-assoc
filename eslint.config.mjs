dcdc6a46a1bad77cb326cd2f652aa757bc94b7b2
MikuroXina <ryosukadnak@gmail.com>
2024-04-14 06:24:43 +0900

    Migrate eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import eslint from "@eslint/js";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tsEslint from "typescript-eslint";
+
+export default tsEslint.config(
+    eslint.configs.recommended,
+    ...tsEslint.configs.recommended,
+    {
+        plugins: {
+            "simple-import-sort": simpleImportSort,
+        },
+        rules: {
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
+        },
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+                ecmaVersion: "latest",
+                sourceType: "module",
+                globals: {
+                    ...globals.node,
+                },
+            },
+            ecmaVersion: 2021,
+        },
+        rules: {
+            "linebreak-style": ["error", "unix"],
+            quotes: ["error", "double"],
+            semi: ["error", "always"],
+        },
+    },
+);
